b94ce52ff144aaddf913df3811fb4370a2db7523
ch8n <chetan.garg36@gmail.com>
2021-07-26 01:51:34 +0530

    :wastebasket: clean-up project

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Netflix-Clone-React
 Practising React by building Netflix Clone
 
+# Requirements
+- [TMDB api key](https://www.themoviedb.org/)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello ch8n! {process.env.REACT_APP_TMDB_API}
     </div>
   );
 }
