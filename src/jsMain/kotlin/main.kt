da44f007cbf616b6def7c81ce9bb6012c8c4290d
ch8n <chetan.garg36@gmail.com>
2021-09-05 12:16:32 +0530

    🦴 Skeleton Setup - added data layer and apis

--- src/jsMain/kotlin/main.kt
+++ src/jsMain/kotlin/main.kt
@@ -1,13 +1,8 @@
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    var count: Int by mutableStateOf(0)
-
     renderComposable(rootElementId = "root") {
         H1 {
             Text("Hello World")
