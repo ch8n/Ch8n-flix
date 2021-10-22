6c08bd59056402f98828bdf0f87e82ebd008320b
ch8n <chetan.garg36@gmail.com>
2021-10-22 22:11:50 +0530

    🧰 api response wip

--- src/main/kotlin/ui/components/PosterItem.kt
+++ src/main/kotlin/ui/components/PosterItem.kt
@@ -1,33 +1,55 @@
 package ui.components
 
 import androidx.compose.runtime.*
+import data.models.PosterResponse
 import data.remote.api.AppApis
 import data.repositories.PosterRepository
 import kotlinx.browser.window
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.await
-import org.jetbrains.compose.web.css.buildCSSStyleRule
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.H3
-import org.jetbrains.compose.web.dom.Text
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.attributes.AttrsBuilder
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.selectors.CSSSelector
+import org.jetbrains.compose.web.css.selectors.attr
+import org.jetbrains.compose.web.dom.*
+import org.w3c.dom.HTMLHeadingElement
 
 
 @Composable
 fun PosterItem() {
 
-    val (msg,setMsg) = remember { mutableStateOf("Api call in process...") }
+    val (msg, setMsg) = remember { mutableStateOf("Api call in process...") }
+    val scope = rememberCoroutineScope()
 
-    LaunchedEffect(Unit) {
-        val repo = PosterRepository()
-        val categories = repo.getCategories()
-        val category = categories.get(0)
-        val categoryFetchUrl = "${AppApis.BASE_URL}${category.categoryFetchUrl}"
-        val response = window.fetch(categoryFetchUrl).await()
+    Button(
+        attrs = {
+            style {
+                color(Color.red)
+            }
+            onClick {
+                scope.launch {
+                    val repo = PosterRepository()
+                    val categories = repo.getCategories()
+                    val category = categories.get(0)
+                    val categoryFetchUrl = "${AppApis.BASE_URL}${category.categoryFetchUrl}"
+                    val posterItems = httpGet<PosterResponse>(categoryFetchUrl)
+                    setMsg(posterItems?.results?.toString()?:"Response failed")
+                }
+            }
+        }
+    ) {
+        Text("retry")
+    }
+    Br()
+    Text(msg)
+}
+
+suspend fun <T> httpGet(url: String): T? {
+    val response = runCatching {
+        val response = window.fetch(url).await()
         val jsonString = JSON.stringify(response.json().await())
-        setMsg(jsonString)
+        JSON.parse(jsonString) as T
     }
-
-    H3 {
-        Text(msg)
-    }
-
+    return response.getOrNull()
 }
