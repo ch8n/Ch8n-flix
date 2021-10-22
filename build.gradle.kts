ca051ec9757801f92ce90563c25982eafd03cc0b
ch8n <chetan.garg36@gmail.com>
2021-10-22 20:57:15 +0530

    🧰 fixed web compose template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,21 @@
 import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 
-// Add compose gradle plugin
 plugins {
-    kotlin("multiplatform") version "1.5.30"
-    id("org.jetbrains.compose") version ("1.0.0-alpha4-build331")
+    kotlin("multiplatform") version "1.5.31"
+    id("org.jetbrains.compose") version ("1.0.0-beta1")
 }
+
 group = "io.github.ch8n"
-version = "1.0.0-alpha01"
+version = "1.0"
 
-// Add maven repositories
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-}
-
-dependencies {
-
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
 }
 
 kotlin {
@@ -26,8 +25,9 @@ kotlin {
     }
     sourceSets {
         val jsMain by getting {
-            kotlin.srcDir("src/jsMain/kotlin")
-            resources.srcDir("src/jsMain/resources")
+            kotlin.srcDir("src/main/kotlin")
+            resources.srcDir("src/main/resources")
+
             dependencies {
                 implementation(compose.web.core)
                 implementation(compose.runtime)
@@ -35,3 +35,12 @@ kotlin {
         }
     }
 }
+
+// a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
+afterEvaluate {
+        rootProject.extensions.configure<NodeJsRootExtension> {
+        versions.webpackDevServer.version = "4.0.0"
+        versions.webpackCli.version = "4.9.0"
+    }
+}
+
